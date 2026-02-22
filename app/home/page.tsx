eb81eb0872231c4c48b913fa9896fb5a31b1bc66
CheackenDesu <col.2023010198@lsb.edu.ph>
2026-02-22 22:44:17 +0800

    homepage-navigation

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -56,11 +56,6 @@ export default function HomePage() {
         <header className="bg-[#2d3f47] border-b border-[#3a4f5a] p-4 sticky top-0 z-10">
           <div className="flex items-center justify-between">
             <h1 className="text-xl font-bold text-white">SkillSwap</h1>
-            <button className="text-gray-400 hover:text-gray-300">
-              <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 24 24">
-                <path d="M15.5 1h-8C6.12 1 5 2.12 5 3.5v17C5 21.88 6.12 23 7.5 23h8c1.38 0 2.5-1.12 2.5-2.5v-17C18 2.12 16.88 1 15.5 1zm-4 21c-.83 0-1.5-.67-1.5-1.5s.67-1.5 1.5-1.5 1.5.67 1.5 1.5-.67 1.5-1.5 1.5zm4.5-4H7V4h8v14z" />
-              </svg>
-            </button>
           </div>
         </header>
 
